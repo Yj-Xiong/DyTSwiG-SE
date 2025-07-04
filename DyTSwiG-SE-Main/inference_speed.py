881a417ad454476d2b76a405c6f4db0f95a20971
Xiong Yujie <58915376+Yj-Xiong@users.noreply.github.com>
2025-07-04 15:03:13 +0800

    Update inference_speed.py

--- DyTSwiG-SE-Main/inference_speed.py
+++ DyTSwiG-SE-Main/inference_speed.py
@@ -78,10 +78,10 @@ def main():
     parser.add_argument('--input_noisy_wavs_dir', default='/home/xyj/Experiments/MP-SENet/VoiceBank+DEMAND/wav_noisy')
     parser.add_argument('--input_test_file', default='/home/xyj/Experiments/MP-SENet/VoiceBank+DEMAND/test.txt')
     parser.add_argument('--output_dir', default='generated_files/g_best')
-    parser.add_argument('--checkpoint_file', default='/home/xyj/Experiments/PrimeK-Net-main/VB_CKPT_test/g_best_PESQ3.5612588650682597_epoch35')
+    parser.add_argument('--checkpoint_file', default='/home/xyj/Experiments/DyTSwiG-SE-main/VB_CKPT_test/g_best_PESQ3.5612588650682597_epoch35')
     a = parser.parse_args()
 
-    config_file = '/home/xyj/Experiments/PrimeK-Net-main/SEMambaconfig.json'
+    config_file = '/home/xyj/Experiments/DyTSwiG-SE-main/SEMambaconfig.json'
     with open(config_file) as f:
         data = f.read()
 
