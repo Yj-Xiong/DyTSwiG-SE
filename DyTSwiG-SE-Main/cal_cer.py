5c316e9482a50e3c6f710a73145f371bb79c011d
Xiong Yujie <58915376+Yj-Xiong@users.noreply.github.com>
2025-07-04 14:47:45 +0800

    Update cal_cer.py

--- DyTSwiG-SE-Main/cal_cer.py
+++ DyTSwiG-SE-Main/cal_cer.py
@@ -141,7 +141,7 @@ def main():
     parser.add_argument('--input_noisy_wavs_dir', default='/home/xyj/datasets/chinese/test_noisy')
     parser.add_argument('--input_test_file', default='/home/xyj/datasets/chinese/chinese_test.txt')
     parser.add_argument('--output_dir', default='/home/xyj/Experience/CMG-v1/Enh_audio-primeknet-zh-43/')
-    parser.add_argument('--checkpoint_file', default='/home/xyj/Experience/g_best_PESQ3.549408779850284_epoch44')
+    parser.add_argument('--checkpoint_file', default='/home/xyj/Experience/g_best_PESQ3.549408779850284_epoch84')
     parser.add_argument('--post_processing_PCS', default=False)
     a = parser.parse_args()
 
